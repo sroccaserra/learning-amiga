a6d921d4f85526df52dec99c2d7c5fce80ba2a21
Sébastien Roccaserra <sroccaserra@yahoo.com>
2018-11-27 23:22:18 +0100

    Bounce the rasterline up and down

--- src/learning.asm
+++ src/learning.asm
@@ -7,12 +7,22 @@ init:
 *******************************
 mainloop:
 waitframe:
-        cmp.b   #$ff,$dff006
+        cmp.b   #$ff,$dff006    ; wait for vpos to equal #$ff
         bne     waitframe
 
 ;------ Frame loop start ------
 
         add     d6,d7           ; increment y position of rasterline
+
+        cmp.b   #$f0,d7
+        blo     ok1             ; is the line lower than #$f0?
+        neg     d6              ; if not, negate increment (bounce on the bottom)
+ok1:
+
+        cmp.b   #$40,d7
+        bhi     ok2
+        neg     d6              ; same idea, but instead bounce on the top (line #$40)
+ok2:
 
 waitras1:
         cmp.b   $dff006,d7
